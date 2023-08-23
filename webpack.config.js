ad5f3a541f22ee2e5be86385086d4c2bb0357ad1
HafizhMA <muhammadazhr29@gmail.com>
2023-08-23 15:56:17 +0700

    konfigurasi webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,29 @@ module.exports = {
     filename: "main.js",
   },
   watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
