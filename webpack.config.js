abf8e8eb62da2f081ebde9a28735b3d01022f599
HafizhMA <muhammadazhr29@gmail.com>
2023-08-24 15:10:08 +0700

    konfigurasi mode developer dan production

--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,13 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "production",
   entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.[contenthash].js",
-    clean: true,
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
-  watch: true,
+  devtool: false,
   module: {
     rules: [
       {
